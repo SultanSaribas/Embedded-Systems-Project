23694ebcd09dfc59655e03ef29f8397423df5500
Melike Doğru <51715369+MelikeDogru@users.noreply.github.com>
2021-06-16 16:48:54 +0300

    motor problem 2

--- Project_v1/Core/Src/main.c
+++ Project_v1/Core/Src/main.c
@@ -263,7 +263,7 @@ int main(void)
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_RESET);
 	}
-	if(hc72_MSB < 32){
+	if(hc72_MSB > 32){
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_13, GPIO_PIN_SET);
 			HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
 	}
@@ -540,18 +540,35 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2|LCD_D7_Pin|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_14|GPIO_PIN_15|LCD_EN_Pin|LCD_RS_Pin
-                          |LCD_D4_Pin|GPIO_PIN_7|LCD_D5_Pin|LCD_D6_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_2|LCD_D7_Pin|GPIO_PIN_13|GPIO_PIN_14
+                          |GPIO_PIN_15|LCD_EN_Pin|LCD_RS_Pin|LCD_D4_Pin
+                          |GPIO_PIN_7|LCD_D5_Pin|LCD_D6_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PB2 LCD_D7_Pin PB12 PB13
-                           PB14 PB15 LCD_EN_Pin LCD_RS_Pin
-                           LCD_D4_Pin PB7 LCD_D5_Pin LCD_D6_Pin */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|LCD_D7_Pin|GPIO_PIN_12|GPIO_PIN_13
-                          |GPIO_PIN_14|GPIO_PIN_15|LCD_EN_Pin|LCD_RS_Pin
-                          |LCD_D4_Pin|GPIO_PIN_7|LCD_D5_Pin|LCD_D6_Pin;
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+
+  /*Configure GPIO pins : PB2 LCD_D7_Pin PB14 PB15
+                           LCD_EN_Pin LCD_RS_Pin LCD_D4_Pin PB7
+                           LCD_D5_Pin LCD_D6_Pin */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|LCD_D7_Pin|GPIO_PIN_14|GPIO_PIN_15
+                          |LCD_EN_Pin|LCD_RS_Pin|LCD_D4_Pin|GPIO_PIN_7
+                          |LCD_D5_Pin|LCD_D6_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB12 */
+  GPIO_InitStruct.Pin = GPIO_PIN_12;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_MEDIUM;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : PB13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
